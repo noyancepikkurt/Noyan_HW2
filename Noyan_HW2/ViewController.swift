a66953e86ff31bcce6ea70a130bbf531e0f81a3d
Noyan Çepikkurt <noyan.cepikkurt@fmsstech.com>
2023-05-10 22:24:23 +0300

    first commit

---
+++ Noyan_HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noyan_HW2
+//
+//  Created by Noyan Çepikkurt on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
